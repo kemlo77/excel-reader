65fc522f595f30e931ec9bfe0f4039e6cc1e90d2
Fredrik Kemling <kemling@gmail.com>
2018-09-20 00:38:49 +0200

    lade till en excel-läsare

---
+++ src/test/java/utils/ExcelReader.java
@@ -0,0 +1,46 @@
+package utils;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class ExcelReader {
+
+
+  public static void main(String[] args) {
+    hamtaRader();
+  }
+
+  public static void hamtaRader(){
+    try(Workbook excelFil = WorkbookFactory.create(
+        new File("./src/test/resources/excel-filer/testdata1.xlsx"),null,true)) {
+
+      System.out.println("Excelfilen har " + excelFil.getNumberOfSheets() + " blad");
+
+      Sheet excelBlad1 = excelFil.getSheet("Blad1");
+      int forstaRaden = excelBlad1.getFirstRowNum();
+      int sistaRaden = excelBlad1.getLastRowNum();
+
+      for (int i = forstaRaden+1; i <= sistaRaden; i++) {
+        Row rad = excelBlad1.getRow(i);
+        for (int j = rad.getFirstCellNum(); j < rad.getLastCellNum(); j++) {
+          System.out.print(rad.getCell(j));
+          System.out.print("\t");
+        }
+        System.out.println("");
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+
+
+
+
+
+
+}
